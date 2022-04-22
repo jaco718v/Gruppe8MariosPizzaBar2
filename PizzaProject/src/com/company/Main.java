bdde59026d9c5b050c942dd0f511cc08c8713c11
Mark <mark771e@stud.kea.dk>
2022-04-22 13:45:29 +0200

    Main.java - Removed: outcommented unused code

--- PizzaProject/src/com/company/Main.java
+++ PizzaProject/src/com/company/Main.java
@@ -1,19 +1,7 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
         new Kontrol().run();
-	      //new Main().run();
     }
-
-    public void run() {
-       //menukort.printMenu();
-    }
-
-
-
 }
