2e2068e9225fd9c8861b1abcafcd16e88f011474
Mark <mark771e@stud.kea.dk>
2022-04-22 13:33:23 +0200

    Kontrol.java - Changed connections from placeholders to actual code

--- PizzaProject/src/com/company/Kontrol.java
+++ PizzaProject/src/com/company/Kontrol.java
@@ -11,20 +11,40 @@ public class Kontrol {
 
     public void run(){
         while(running) {
+            System.out.println("""
+                ------------------------------------------------------------------
+                1. 'input' - for at lægge en bestilling ind
+                2. 'ekspedere + <ordreNummer>'- for at fjerne en ordre til ekspederet
+                3. 'menu' - for at vise pizza menu'en
+                4. 'stop' - for at stoppe programmet
+                ------------------------------------------------------------------
+                """);
+            System.out.println("nuværrende ordre:");
+            oh.visBestillingsListe();
+
+            System.out.println("ekspederet ordre:");
+            oh.visEkspederetListe();
             valg();
         }
     }
 
     private void valg(){
-        switch (keyboard.nextLine().toLowerCase()) {
-            case "input" -> System.out.println("input");
-            case "fjern" -> System.out.println("fjern");
-            case "menu" -> mk.printMenu();
-            case "stop" -> running = false;
+        String[] input = keyboard.nextLine().toLowerCase().split(" ",2);
+        switch (input[0]) {
+            case "1","input" -> oh.opretBestilling(mk);
+            case "2","ekspedere" -> {
+
+                // ParseInt problemstilling hvis Alfonso skriver andet end tal
+                if (input.length == 1 || input[1].isEmpty()) {
+                    System.out.println("Du mangler at skrive et tal efterfølgende.");
+                } else {
+                    oh.færdiggørOrdre(Integer.parseInt(input[1]));
+                }
+            }
+            case "3","menu" -> mk.printMenu();
+            case "4","stop" -> running = false;
             default -> valg();
         }
     }
-
-
 
 }
